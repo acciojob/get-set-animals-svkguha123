6e014ace98d2c02ab1418c0cf1d2a67f2da16374
Souvik Guha <svkguha123@gmail.com>
2023-04-28 12:38:32 +0530

    up

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-//complete this code
 class Animal {
-    constructor(species){
-        this.species = species;
+    constructor(species) {
+      this._species = species;
     }
     get species() {
-        return this.species;
+      return this._species;
     }
     makeSound() {
-        console.log(this.species+" make a sound");
+      console.log("The " + this.species + " makes a sound");
     }
-} 
-
-class Cat extends Animal {
-    purr(){
-        console.log("purr");
+  }
+  class Cat extends Animal {
+    purr() {
+      console.log("purr");
     }
-}
-
-class Dog extends Animal {
-    bark(){
-        console.log("woof");
+  }
+  class Dog extends Animal {
+    bark() {
+      console.log("woof");
     }
-}
-
-
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
-
+  }
+  // Do not change the code below this line
+  window.Animal = Animal;
+  window.Dog = Dog;
+  window.Cat = Cat;
+  
+  function getElementsByAttribute(attribute, value) {
+    const elementsWithAttribute = [];
+    const allElements = document.getElementsByTagName("*");
+  
+    for (let i = 0; i < allElements.length; i++) {
+      if (allElements[i].getAttribute(attribute) === value) {
+        elementsWithAttribute.push(allElements[i]);
+      }
+    }
+  
+    return elementsWithAttribute;
+  }
+   const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+      console.log(elementsWithBarBaz);
